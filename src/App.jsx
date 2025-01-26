c2fae338b28518d6fff1e0dec5be5c28a8e1e495
chankaran2996 <chandrasekaran2996@gmail.com>
2025-01-26 22:43:00 +0530

    4.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-      check
+    <div>
+      <nav>Navbar</nav>
+      <main>
+        <Outlet/>
+      </main>
+      <footer>Footer</footer>
     </div>
   )
 }
