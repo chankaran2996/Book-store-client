225973ddaff563621aa72638cace29bcd3f4afc6
chankaran2996 <chandrasekaran2996@gmail.com>
2025-02-01 23:38:16 +0530

    9.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <nav>Navbar</nav>
+      <Navbar/>
       <main className='min-h-screen max-w-screen-2xl mx-auto p-4 py-6'>
         <Outlet/>
       </main>
